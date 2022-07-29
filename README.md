0c30a51768c5891f5e4eb3fa5b4ccd8abac9c10b
j31234 <j31234@126.com>
2022-07-29 23:54:17 +0800

    Xterm.js socket api demo (for single user)

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+### **注意**：请在linux环境下运行，`pty, select`模块在Windows下不能正常工作
+
 * `flask run`启动应用
+
+* 或是用`python app.py`以调试模式启动应用
 
 * 可以更改`views`下的文件，`views/{module}.py`的API将放在`localhost:5000/{module}`前缀下（具体参见`app.py`中的`app.register_blueprint`语句）。
 
--- app.py
+++ app.py
@@ -1,17 +1,54 @@
 from flask import Flask
 from views.docker import docker_bp
-from views.socket import socket_bp
 from views.database import database_bp
 from views.login import login_bp
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
-app.register_blueprint(docker_bp, url_prefix='/docker')
-app.register_blueprint(socket_bp, url_prefix='/socket')
-app.register_blueprint(database_bp, url_prefix='/database')
-app.register_blueprint(login_bp, url_prefix='/login')
+app.register_blueprint(docker_bp, url_prefix="/docker")
+app.register_blueprint(database_bp, url_prefix="/database")
+app.register_blueprint(login_bp, url_prefix="/login")
+
+app.config["SECRET_KEY"] = "secret!qwq"
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+# xterm.js socket
+import pty
+import select
+import os
+import subprocess
+
+
+def send_worker():
+    while socketio.terminal.poll() is None:
+        r, _, _ = select.select([socketio.pty], [], [])
+        if socketio.pty in r:
+            output_from_docker = os.read(socketio.pty, 1024)
+            socketio.emit("response", output_from_docker.decode())
+
+
+@socketio.on("connect")
+def init_terminal():
+    socketio.pty, tty = pty.openpty()
+    socketio.terminal = subprocess.Popen(
+        ["docker", "exec", "-it", "6a8", "bash"], stdin=tty, stdout=tty, stderr=tty
+    )
+
+    socketio.start_background_task(send_worker)
+
+
+@socketio.on("message")
+def handle_message(data):
+    print("received message: " + data)
+    os.write(socketio.pty, data.encode())
+
+
+if __name__ == "__main__":
+    socketio.run(app, port=5000, debug=True)
