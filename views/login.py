29c5dcfcfd9188406d7b2010d928bf9557a7e60c
wushusuoshuweishu <84722564+wushusuoshuweishu@users.noreply.github.com>
2022-08-01 18:34:44 +0800

    Update login.py

--- views/login.py
+++ views/login.py
@@ -1,10 +1,63 @@
 from flask import Blueprint
 
+from database import db_insert, db_verify_pw, db_del
+from flask import session, Flask, request, redirect, url_for, render_template
+from flask_wtf import CSRFProtect
+from form import RegistrationForm, DeRegistrationForm, LoginForm
+
 
 login_bp = Blueprint("login", __name__)
 
 
-# use blueprint as app
-@login_bp.route("/")
-def login_index():
-    return "Login Index"
+def is_login():
+    if 'username' in session:
+        return f'{session["username"]}'
+    else:
+        return None
+
+
+@login_bp.route('/')
+def index():
+    if is_login():
+        return render_template('index.html')
+    return redirect(url_for('login'))
+
+
+@login_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        form = LoginForm()
+        if not form.validate_on_submit():
+            return render_template('login.html', form=form)
+        if db_verify_pw(form.username.data, form.password.data):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@login_bp.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+@login_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if not form.validate_on_submit():
+        return render_template('register.html')
+
+    db_insert(form.username.data, form.password.data)
+    return redirect(url_for('login'))
+
+
+@login_bp.route('/deregister', methods=['GET', 'POST'])
+def deregister():
+    form = DeRegistrationForm()
+    if form.validate_on_submit():
+        if db_del(form.username.data, form.password.data):
+            return redirect(url_for('login'))
+    return render_template('deregister.html')
