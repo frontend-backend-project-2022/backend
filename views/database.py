49db80064948f29914378aa373cf6eb7ca527cb2
wushusuoshuweishu <84722564+wushusuoshuweishu@users.noreply.github.com>
2022-08-16 13:30:47 +0800

    Update database.py

--- views/database.py
+++ views/database.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Blueprint, Flask, request, session, jsonify
 import sqlite3 as sql
 from views.dockers import docker_connect,docker_rm
 from werkzeug.security import generate_password_hash, check_password_hash
